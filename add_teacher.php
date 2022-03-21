5b3bc3df44d96bf32bba8c4f61ea3a139bfc8c11
enamulrafti <enamul.rafti@gmail.com>
2022-03-21 09:15:04 +0600

    add_teacher added

---
+++ add_teacher.php
@@ -0,0 +1,17 @@
+<h1>Add teacher</h1>
+
+
+
+<form method=get action=add_teacher_back.php>
+
+	Name: <input type=text name=name> <br>
+
+	<p>
+
+	Course ID: <input type=text name=course_id> <br>
+
+	<p>
+
+	<input type=submit value=Insert>
+
+</form>
